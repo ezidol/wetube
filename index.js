4ae676e518d247e706db672e8b55302799dcc31c
Suh <shaning7@gmail.com>
2020-06-01 01:08:13 +0900

    2nd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello");
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => console.log(`https://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("This is my profile!!");
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
