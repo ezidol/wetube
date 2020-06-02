b5b1a4a4ffe0ebdef4367d22d0e6959a50674273
ezidol <goodyoungmin@naver.com>
2020-06-02 17:05:39 +0900

    studying

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
+import routes from "../routes";
+import { changePassword, editProfile, userDetail, users } from "../Controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
+
 export default userRouter;
+
