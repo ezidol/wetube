ea8e4a5fa86b5f1d71f599c4e4b9a657a78335bc
Suh <shaning7@gmail.com>
2020-06-09 00:52:00 +0900

    learning controller

--- Controllers/videoController.js
+++ Controllers/videoController.js
@@ -1,5 +1,9 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) => res.render("search", { pageTitle: "Home" });
+export const search = (req, res) =>{
+    const {query: {term: searchingBy }} =req;
+    res.render("search", { pageTitle: "Home", searchingBy });
+}
+ 
 export const videos = (req, res) => res.send("videos");
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
